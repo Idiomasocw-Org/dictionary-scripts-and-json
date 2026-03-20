6dd22e55a2bc889e895360468c6551de1bf07e8b
barbara arias <barbaraarias844@gmail.com>
2026-03-19 19:47:54 -0500

    Add Minions ep1 JSON and fix generate script

--- generate-vocab-csv.js
+++ generate-vocab-csv.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
 
 /**
  * Generates a CSV file from a word list file
